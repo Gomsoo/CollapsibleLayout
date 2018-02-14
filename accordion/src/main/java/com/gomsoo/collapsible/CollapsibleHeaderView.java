47e25845b1697263f3499205a130f481bb9e9260
Gomsoo <suprmoony3@gmail.com>
2018-02-14 16:44:06 +0900

    Add expand and collapse listener

---
+++ accordion/src/main/java/com/gomsoo/collapsible/CollapsibleHeaderView.java
@@ -0,0 +1,8 @@
+package com.gomsoo.collapsible;
+
+/**
+ *
+ * Created by Moon on 2018-02-14.
+ */
+public class CollapsibleHeaderView {
+}
--- accordion/src/main/java/com/gomsoo/collapsible/CollapsibleLayout.java
+++ accordion/src/main/java/com/gomsoo/collapsible/CollapsibleLayout.java
@@ -1,7 +1,6 @@
 package com.gomsoo.collapsible;
 
 import android.content.Context;
-import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -24,6 +23,19 @@ public class CollapsibleLayout extends FrameLayout {
     private int mExpandedParamsHeight;
 
     private View mMarkView;
+
+    private OnExpandListener mOnExpandListener;
+    private OnCollapseListener mOnCollapseListener;
+
+    public interface OnExpandListener {
+        void onExpandStarted(CollapsibleLayout layout);
+        void onExpandEnded(CollapsibleLayout layout);
+    }
+
+    public interface OnCollapseListener {
+        void onCollapseStarted(CollapsibleLayout layout);
+        void onCollapseEnded(CollapsibleLayout layout);
+    }
 
     public CollapsibleLayout(@NonNull Context context) {
         super(context);
@@ -71,40 +83,58 @@ public class CollapsibleLayout extends FrameLayout {
         collapse(mAnimationDurationInMillis);
     }
 
-    private void collapse(long durationInMillis) {
+    public void collapse(long durationInMillis) {
         if (mExpandedHeight == 0 || mExpandedParamsHeight == 0) {
             mExpandedHeight = getHeight();
             mExpandedParamsHeight = getLayoutParams().height;
         }
         mIsCollapse = true;
-        startAnimation(new CollapseAnimation(this, mMarkView, durationInMillis));
+        startAnimation(new CollapseAnimation(
+                this, mMarkView, durationInMillis, mOnCollapseListener));
     }
 
     public void expand() {
         expand(mAnimationDurationInMillis);
     }
 
-    private void expand(long durationInMillis) {
+    public void expand(long durationInMillis) {
         mIsCollapse = false;
-        startAnimation(new ExpandAnimation(
-                this, mExpandedHeight, mExpandedParamsHeight, mMarkView, durationInMillis));
+        startAnimation(new ExpandAnimation(this, mExpandedHeight,
+                mExpandedParamsHeight, mMarkView, durationInMillis, mOnExpandListener));
+    }
+
+    public void setOnExpandListener(OnExpandListener listener) {
+        mOnExpandListener = listener;
+    }
+
+    public void setOnCollapseListener(OnCollapseListener listener) {
+        mOnCollapseListener = listener;
     }
 
     private static class CollapseAnimation extends Animation {
-        private final View mTargetView;
+
+        private final CollapsibleLayout mTargetView;
         private final ViewGroup.LayoutParams mTargetParams;
         private final int mInitialHeight;
 
         private final View mMarkView;
         private final float mInitialRotation;
 
-        private CollapseAnimation(View targetView, View markView, long durationInMillis) {
+        private final OnCollapseListener mListener;
+        private boolean mIsNotifiedStart;
+        private boolean mIsNotifiedEnd;
+
+        private CollapseAnimation(CollapsibleLayout targetView, View markView,
+                                  long durationInMillis, OnCollapseListener listener) {
             mTargetView = targetView;
             mTargetParams = targetView.getLayoutParams();
             mInitialHeight = targetView.getHeight();
             mMarkView = markView;
             mInitialRotation = markView == null ? 0 : markView.getRotation();
             setDuration(durationInMillis);
+            mListener = listener;
+            mIsNotifiedStart = false;
+            mIsNotifiedEnd = false;
 
             // targetView의 parent가 invalidate 되지 않아 최초 한 번 이후 applyTransformation이
             // 콜 되지 않는 현상이 있음
@@ -122,15 +152,26 @@ public class CollapsibleLayout extends FrameLayout {
                 mMarkView.setRotation(
                         mInitialRotation * (1 - interpolatedTime) + -90f * interpolatedTime);
 
+            if (interpolatedTime == 0f) {
+                if (mListener != null && !mIsNotifiedStart) {
+                    mListener.onCollapseStarted(mTargetView);
+                    mIsNotifiedStart = true;
+                }
+            }
+
             if (interpolatedTime == 1f) {
                 mTargetView.setVisibility(View.GONE);
+                if (mListener != null && !mIsNotifiedEnd) {
+                    mListener.onCollapseEnded(mTargetView);
+                    mIsNotifiedEnd = true;
+                }
             }
         }
     }
 
     private static class ExpandAnimation extends Animation {
 
-        private final View mTargetView;
+        private final CollapsibleLayout mTargetView;
         private final ViewGroup.LayoutParams mTargetParams;
         private final int mInitialHeight;
 
@@ -140,8 +181,12 @@ public class CollapsibleLayout extends FrameLayout {
         private final View mMarkView;
         private final float mInitialRotation;
 
-        private ExpandAnimation(View targetView, int expandedHeight, int expandedParamsHeight,
-                                View markView, long durationInMillis) {
+        private final OnExpandListener mListener;
+        private boolean mIsNotifiedStart;
+        private boolean mIsNotifiedEnd;
+
+        private ExpandAnimation(CollapsibleLayout targetView, int expandedHeight, int expandedParamsHeight,
+                                View markView, long durationInMillis, OnExpandListener listener) {
             mTargetView = targetView;
             mTargetParams = targetView.getLayoutParams();
             mInitialHeight = targetView.getHeight();
@@ -150,6 +195,9 @@ public class CollapsibleLayout extends FrameLayout {
             mMarkView = markView;
             mInitialRotation = markView == null ? 0 : markView.getRotation();
             setDuration(durationInMillis);
+            mListener = listener;
+            mIsNotifiedStart = false;
+            mIsNotifiedEnd = false;
 
             // targetView의 parent가 invalidate 되지 않아 최초 한 번 이후 applyTransformation이
             // 콜 되지 않는 현상이 있음
@@ -168,9 +216,20 @@ public class CollapsibleLayout extends FrameLayout {
                 mMarkView.setRotation(
                         mInitialRotation * (1 - interpolatedTime) + 0 * interpolatedTime);
 
+            if (interpolatedTime == 0f) {
+                if (mListener != null && !mIsNotifiedStart) {
+                    mListener.onExpandStarted(mTargetView);
+                    mIsNotifiedStart = true;
+                }
+            }
+
             if (interpolatedTime == 1f) {
                 mTargetParams.height = mExpandedParamsHeight;
                 mTargetView.requestLayout();
+                if (mListener != null && !mIsNotifiedEnd) {
+                    mListener.onExpandEnded(mTargetView);
+                    mIsNotifiedEnd = true;
+                }
             }
         }
     }
